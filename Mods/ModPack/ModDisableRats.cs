a8e36fa4f21ccb6b0c0bcc274ef96f721170e679
robot9706 <robot9706@gmail.com>
2017-06-07 21:02:50 +0200

    New mod

---
+++ Mods/ModPack/ModDisableRats.cs
@@ -0,0 +1,15 @@
+﻿using FSLoader;
+
+namespace ModPack
+{
+    [ModInfo("disable_rats", "Disable rats", "Robot9706", 1, 0)]
+    public class ModDisableRats : Mod
+    {
+        [Hook("RatsManager::IsRatForbiddenRoom(ERoomType)")]
+        public void Hook_Rats(CallContext context, ERoomType room)
+        {
+            context.IsHandled = true;
+            context.ReturnValue = true;
+        }
+    }
+}
