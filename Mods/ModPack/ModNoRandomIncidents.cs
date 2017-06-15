ff55fb62a2d85f3c064f2359d2cfdaad2c54effb
robot9706 <robot9706@gmail.com>
2017-06-15 23:34:46 +0200

    Mods

---
+++ Mods/ModPack/ModNoRandomIncidents.cs
@@ -0,0 +1,14 @@
+﻿using FSLoader;
+
+namespace ModPack
+{
+    [ModInfo("no_random_incidents", "No random incidents", "Robot9706", 1, 0)]
+    public class ModNoRandomIncidents : Mod
+    {
+        [Hook("VaultEmergencyState::CheckRandomEmergency(System.Boolean)")]
+        public void Hook_Check(CallContext context, bool online)
+        {
+            context.IsHandled = true;
+        }
+    }
+}
